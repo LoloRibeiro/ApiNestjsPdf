04905c94ebb50c74193c8abef9674d67f9d6a41d
Berthe-Ribeiro Laura <laura.berthe-ribeiro@epitech.eu>
2020-02-09 00:08:39 +0100

    create-a-root-data-text

--- api-nestjs-pdf/src/services/data/data.service.ts
+++ api-nestjs-pdf/src/services/data/data.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class DataService {}
+export class DataService {
+
+    getTextFromPdf = async (fileBuffer: string) => {
+        const pdf = require('pdf-parse');
+        return (pdf(fileBuffer).then(function(data) {
+            return data.text;
+        }).catch(function(error) {
+            return (error);
+        }));
+    };
+}
