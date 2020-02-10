ed5b5796af353b6ad1716759f2982c6ad4549afc
Berthe-Ribeiro Laura <laura.berthe-ribeiro@epitech.eu>
2020-02-10 23:52:02 +0100

    create root data infos

--- api-nestjs-pdf/src/services/data/data.service.ts
+++ api-nestjs-pdf/src/services/data/data.service.ts
@@ -17,7 +17,16 @@ export class DataService {
         return (pdf(fileBuffer).then(function(data) {
             return data.numrender;
         }).catch(function(error) {
-            return error
+            return error;
+        }));
+    }
+
+    getInfoPdf = async (fileBuffer: string) => {
+        const pdf = require('pdf-parse');
+        return (pdf(fileBuffer).then(function(data) {
+            return data.info;
+        }).catch(function(error) {
+            return error;
         }));
     }
 }
