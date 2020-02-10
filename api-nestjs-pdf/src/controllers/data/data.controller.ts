91593bd52381801d645a67406aac1c288958117e
Berthe-Ribeiro Laura <laura.berthe-ribeiro@epitech.eu>
2020-02-10 22:58:42 +0100

    create root data numpage

--- api-nestjs-pdf/src/controllers/data/data.controller.ts
+++ api-nestjs-pdf/src/controllers/data/data.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get, UploadedFile, UseInterceptors, Res} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FileFieldsInterceptor } from '@nestjs/platform-express';
 import { DataService } from '../../services/data/data.service';
 import { Response } from 'express';
+import { fileURLToPath } from 'url';
 
 @Controller('data')
 export class DataController {
@@ -11,8 +12,16 @@ export class DataController {
     //Root "data/text" take a file in parameters and return the text of the file
     @Get('text')
     @UseInterceptors(FileInterceptor('file'))
-    async uploadFile(@UploadedFile() file, @Res() res: Response) {
+    async sendTextPdf(@UploadedFile() file, @Res() res: Response) {
         const textPdf = await this.dataService.getTextFromPdf(file.buffer);
         res.send({textPdf}); 
     };
+
+    //Root "data/numpage" take the number of page in a file pdf
+    @Get('numpage')
+    @UseInterceptors(FileInterceptor('file'))
+    async sendNumPagePdf(@UploadedFile() file, @Res() res: Response) {
+        const numPagePdf = await this.dataService.getNumPagePdf(file.buffer);
+        res.send({numPagePdf});
+    }
 }
